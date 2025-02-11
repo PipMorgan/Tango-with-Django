d2edf68b54bf020c6fd5dbd18ad4084e63d9399d
Philippa Morgan <pipskye6@gmail.com>
2025-02-11 12:18:08 +0000

    Chapter 10.5 complete

--- rango/views.py
+++ rango/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
+from datetime import datetime
 
 
 
@@ -17,15 +18,12 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages']=page_list
 
-    request.session.set_test_cookie()
+    response = render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request, response)
 
-    return render(request, 'rango/index.html', context=context_dict)
+    return response
 
 def about(request):
-    if request.session.test_cookie_worked():
-        print("TEST COOKIE WORKED!")
-        request.session.delete_test_cookie
-
     return render(request, 'rango/about.html')
 
 def show_category(request, category_name_slug):
@@ -135,3 +133,17 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
+
+
+# Helper function
+def visitor_cookie_handler(request, response):
+    visits = int(request.COOKIES.get('visits', '1'))
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days>0:
+        visits +=1
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        response.set_cookie('last_visit', last_visit_cookie)
+    response.set_cookie('visits', visits)
